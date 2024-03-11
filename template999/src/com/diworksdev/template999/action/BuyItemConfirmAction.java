80ebf5de858d4916efab53064a63c8cbd2ee2adb
ssskkk17 <not36verstile@gmail.com>
2024-03-11 18:53:41 +0900

    task5 finished

--- template999/src/com/diworksdev/template999/action/BuyItemConfirmAction.java
+++ template999/src/com/diworksdev/template999/action/BuyItemConfirmAction.java
@@ -16,7 +16,7 @@ public class BuyItemConfirmAction extends ActionSupport implements SessionAware 
 				session.get("id").toString(),
 				session.get("buyItem_price").toString(),
 				session.get("stock").toString(),
-				session.get("login_user_id").toString(),
+				session.get("loginUserId").toString(),
 				session.get("pay").toString());
 				
 				String result = SUCCESS;
